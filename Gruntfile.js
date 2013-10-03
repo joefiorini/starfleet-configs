91c4b559d6a812eebf4f35eb9ea9ebba0ad16861
Joe Fiorini <joe@joefiorini.com>
2013-10-02 23:16:49 -0400

    Add s3 and bump commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
+    aws: grunt.file.readJSON(process.env.HOME + '/grunt-aws.json'),
     pkg: grunt.file.readJSON('package.json'),
     compress: {
       main: {
@@ -17,8 +18,31 @@ module.exports = function(grunt) {
         }]
       }
     },
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        access: 'public-read',
+        bucket: 'configs.static.triforce.io'
+      },
+      master: {
+        upload: [{
+          src: '_dist/configs-*.tar.gz'
+        }]
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        commitMessage: 'Release v%VERSION%. Woot. :tada:',
+        pushTo: 'origin',
+        updateConfigs: ['pkg']
+      }
+    },
     clean: ['_dist']
   });
 
+  grunt.registerTask('push', ['s3:master']);
+  grunt.registerTask('release', ['bump', 'push']);
   grunt.registerTask('package', ['clean', 'compress']);
 };
